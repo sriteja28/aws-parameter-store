b752269aef6ceadf9c337e91636e609ef4140be9
Sriteja Kattamuru <131655915+sriteja28@users.noreply.github.com>
2023-09-30 14:29:09 +0530

    Playbook

--- main.tf
+++ main.tf
@@ -27,7 +27,7 @@ variable "params" {
     { name= "roboshop.dev.shipping.db_host", value= "mysql-dev.sritejadevops.com", type= "String" },
     { name= "roboshop.dev.user.mongo", value= "true", type= "String" },
     { name= "roboshop.dev.user.redis_host", value= "redis-dev.sritejadevops.com", type= "String" },
-    { name= "roboshop.dev.user.mongo_url", value= "mongodb://mongodb-{{env}}.sritejadevops.com:27017/users", type= "String" },
+    { name= "roboshop.dev.user.mongo_url", value= "mongodb://mongodb-dev.sritejadevops.com:27017/users", type= "String" },
 
 
 
